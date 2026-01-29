137e704535edcd10468ce08b8e8b21c1523caa56
Rachiͤt Takate <49189777+Bluejet007@users.noreply.github.com>
2026-01-29 09:57:06 +0530

    Remove unused variables in week 3

--- Week3/Q4.c
+++ Week3/Q4.c
@@ -10,7 +10,6 @@ int main(int argc, char* argv[]) {
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Status stat;
 
     if(!rank) {
         gets(inp1);
