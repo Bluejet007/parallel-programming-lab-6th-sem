2e4e2f51e8609c7a72d54c368f4d614c827bb169
Rachiͤt Takate <49189777+Bluejet007@users.noreply.github.com>
2026-01-29 11:05:31 +0530

    Update boilerplate code template

--- MPI_BoilerPlate.c
+++ MPI_BoilerPlate.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <mpi.h>
+#define MCW MPI_COMM_WORLD
 
-int main(int argc, char* argv[]) {
+int errHandler(int errCode, int rank) {
+    char errStr[MPI_MAX_ERROR_STRING];
+    int errClass, errLen;
+
+    switch (errCode) {
+    case MPI_SUCCESS:
+        return 0;
+    default:
+        MPI_Error_class(errCode, &errClass);
+        MPI_Error_string(errCode, errStr, &errLen);
+        printf("P%d: %s\n", rank, errStr);
+        return 1;
+    }
+}
+
+int main(int argc, char *argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MCW, &rank);
+    MPI_Comm_size(MCW, &size);
 
     /* Code here */
 
-    if(rank == 0)
+    if(!rank)
         printf("Rachit 230962294\n");
     MPI_Finalize();
     return 0;
